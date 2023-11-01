0ce16a93a4c147f3f14d3772036fda6d1d550972
ISM42 <133287937+ISM42@users.noreply.github.com>
2023-10-31 22:56:30 -0600

    Rutas corregidas
    El error estaba en las comillas y faltaba agregar la carpeta donde se encuentran las vistas especificadas.

--- PROYECTO/app/Http/Controllers/ImportacionesController.php
+++ PROYECTO/app/Http/Controllers/ImportacionesController.php
@@ -13,70 +13,70 @@ class ImportacionesController extends Controller
     }
 
     Public function metodoAltaProv(){ 
-        return view(‘alta_proveedor’); 
+        return view('altas.alta_proveedor'); 
     }
 
     Public function metodoRegistroOrdenC(){ 
-        return view(‘registrar_orden_compra’); 
+        return view('altas.registrar_orden_compra'); 
     }
 
     Public function metodoRegistrarProd(){ 
-        return view(‘registrar_producto’); 
+        return view('altas.registrar_producto'); 
     }
 
     Public function metodoRegistrarUsuario(){ 
-        return view(‘registrar_usuario’); 
+        return view('altas.registrar_usuario'); 
     }
 
     Public function metodoRegistrarVenta(){ 
-        return view(‘registrar_venta’); 
+        return view('altas.registrar_venta'); 
     }
 
     Public function metodoConsultaComprasG(){ 
-        return view(‘consultar_compras_gerente’); 
+        return view('consultas.consultar_compras_gerente'); 
     }
 
     Public function metodoConsultaGananciasG(){ 
-        return view(‘consultar_ganancias_gerente’); 
+        return view('consultas.consultar_ganancias_gerente'); 
     }
 
     Public function metodoConsultarGanancias(){ 
-        return view(‘consultar_ganancias’); 
+        return view('consultas.consultar_ganancias'); 
     }
 
     Public function metodoConsultarOrdenC(){ 
-        return view(‘consultar_orden_compra’); 
+        return view('consultas.consultar_orden_compra'); 
     }
 
     Public function metodoConsultarProd(){ 
-        return view(‘consultar_producto’); 
+        return view('consultas.consultar_producto'); 
     }
 
     Public function metodoConsultarProv(){ 
-        return view(‘consultar_proveedor’); 
+        return view('consultas.consultar_proveedor'); 
     }
 
     Public function metodoConsultarTickets(){ 
-        return view(‘consultar_tickets_venta’); 
+        return view('consultas.consultar_tickets_venta'); 
     }
 
     Public function metodoConsultarUsuarios(){ 
-        return view(‘consultar_usuarios’); 
+        return view('consultas.consultar_usuarios'); 
     }
 
     Public function metodoConsultarVentasG(){ 
-        return view(‘consultar_ventas_gerente’); 
+        return view('consultas.consultar_ventas_gerente'); 
     }
 
     Public function metodoEditarProducto(){ 
-        return view(‘editar_producto’); 
+        return view('editar.editar_producto'); 
     }
 
     Public function metodoEditarProveedor(){ 
-        return view(‘editar_proveedor’); 
+        return view('editar.editar_proveedor'); 
     }
 
     Public function metodoEditarUsuario(){ 
-        return view(‘editar_usuario’); 
+        return view('editar.editar_usuario'); 
     }
 }
